7ca3aed8a5bf9e8e6e7e6735a7b3c0d3a164c2f0
Sigit Prasetyo Karisma Utomo <sigitprasetyokarismautomo@gmail.com>
2025-10-29 18:26:06 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\Public\HomeController::class, 'home'])->name('home');
-Route::get('/about', [App\Http\Controllers\Public\AboutController::class, 'about'])->name('about');
-Route::get('/participant', [App\Http\Controllers\Public\ParticipantController::class, 'participant'])->name('participant');
-Route::get('/participant/{slug}', [App\Http\Controllers\Public\ParticipantController::class, 'participantDetail'])->name('participant.detail');
-Route::get('/sponsor', [App\Http\Controllers\Public\SponsorController::class, 'sponsor'])->name('sponsor');
-Route::get('/schedule', [App\Http\Controllers\Public\ScheduleController::class, 'schedule'])->name('schedule');
-Route::get('/registration', [App\Http\Controllers\Public\RegistrationController::class, 'registration'])->name('registration');
+Route::get('/', [App\Http\Controllers\Public\HomeController::class, 'index'])->name('home');
+Route::get('/about', [App\Http\Controllers\Public\AboutController::class, 'index'])->name('about');
+Route::get('/participant', [App\Http\Controllers\Public\ParticipantController::class, 'index'])->name('participant');
+Route::get('/participant/{slug}', [App\Http\Controllers\Public\ParticipantController::class, 'detail'])->name('participant.detail');
+Route::get('/sponsor', [App\Http\Controllers\Public\SponsorController::class, 'index'])->name('sponsor');
+Route::get('/schedule', [App\Http\Controllers\Public\ScheduleController::class, 'index'])->name('schedule');
+Route::get('/registration', [App\Http\Controllers\Public\RegistrationController::class, 'index'])->name('registration');
 Route::post('/registration', [App\Http\Controllers\Public\RegistrationController::class, 'submit'])->name('registration.submit');
 Route::get('/confirmation', [App\Http\Controllers\Public\RegistrationController::class, 'confirmation'])->name('confirmation');
